b7eaf8ee347c2f6694d600cb239673151c7e984e
Edylson Torikai <tedylson@gmail.com>
2019-05-29 23:04:44 -0300

    Fix test matrix & implement shift&pad

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Image steganography
 Edylson Torikai - 5248962  
 
 **Abstract**:  
-Apply steganography techniques to embed information inside a given image. This project intends to apply DCT (Discrete Cosine Transform) techniques as a method to hide information in the frequency domain of the image. The idea will be to hide binary data into a image. There might have a method to extract the data back as well.  
+Apply steganography techniques to embed information inside a given image. This project intends to apply DCT (Discrete Cosine Transform) techniques as a method to hide information in the frequency domain of the image. The idea will be to hide binary data into a image (Will be trying to use text data at first). There might have a method to extract the data back as well.  
   
 This method will consist of using DCT II (type 2) to on each 8x8 pixel-block of the image in order to calculate the weight coeficient for each of the 64 cosine pattern, described in the image below:  
 ![DCT cosine patterns](https://upload.wikimedia.org/wikipedia/commons/2/23/Dctjpeg.png)
@@ -15,5 +15,7 @@ In short, we will try to represent the same 8x8 pixels using only cosines functi
 After finding out the coeficients, we will quantize these using some standard quantization table for JPEG images. This step help us see which patterns have more weight on the overall representation so that we can "discard" some information from the image without changing too much its content. Although the image was changed, it would be barely noticible to the naked eyes.  
 
 From this idea, we will use the fact that high-frequencies cosine functions have lower impact to hide information in these.
+
+Initial tests will be applied on gray-level images, then we will try using RGB images, where DCT will be applied on each color component (this will let us hide bigger messages).
 
 Image source from [Image Database](http://www.imageprocessingplace.com/DIP-3E/dip3e_book_images_downloads.htm) from the book Digital Image Processing 3rd edition, chapter 8
--- proj.py
+++ proj.py
@@ -3,28 +3,28 @@ import imageio
 import matplotlib.pyplot as plt
 from scipy.fftpack import dctn, idctn
 
-# 50% resol.
-# QUANTIZATION_TABLE = np.array([
-#   [16,11,10,16,24,40,51,61],
-#   [12,12,14,19,26,58,60,55],
-#   [14,13,16,24,40,57,69,56],
-#   [14,17,22,29,51,87,80,62],
-#   [18,22,37,56,68,109,103,77],
-#   [24,35,55,64,81,104,113,92],
-#   [49,64,78,87,103,121,120,101],
-#   [72,92,95,98,112,100,103,99]
-# ])
-
+# 50% quality factor
 QUANTIZATION_TABLE = np.array([
-  [16,12,14,14,18,24,49,72],
-  [11,12,13,17,22,35,64,92],
-  [10,14,16,22,37,55,78,95],
-  [16,19,24,29,56,64,87,98],
-  [24,26,40,51,68,81,103,112],
-  [40,58,57,87,109,104,121,100],
-  [51,60,69,80,103,113,120,103],
-  [61,55,56,62,77,92,101,99]
+  [16,11,10,16,24,40,51,61],
+  [12,12,14,19,26,58,60,55],
+  [14,13,16,24,40,57,69,56],
+  [14,17,22,29,51,87,80,62],
+  [18,22,37,56,68,109,103,77],
+  [24,35,55,64,81,104,113,92],
+  [49,64,78,87,103,121,120,101],
+  [72,92,95,98,112,100,103,99]
 ])
+
+# QUANTIZATION_TABLE = np.array([
+#   [16,12,14,14,18,24,49,72],
+#   [11,12,13,17,22,35,64,92],
+#   [10,14,16,22,37,55,78,95],
+#   [16,19,24,29,56,64,87,98],
+#   [24,26,40,51,68,81,103,112],
+#   [40,58,57,87,109,104,121,100],
+#   [51,60,69,80,103,113,120,103],
+#   [61,55,56,62,77,92,101,99]
+# ])
 
 np.set_printoptions(precision=2, suppress=True)
 
@@ -33,32 +33,59 @@ def img_dct(img, q_table):
   assert q_table.shape == (8,8), "q_table parameter should be a 8x8 matrix"
 
   # Shift the whole image, so we can have values centered in 0
-  print(img)
   img = np.subtract(img, 128)
 
-  print(img)
-
   # We will work on 8x8 blocks of the image
+  # Thus, we will pad the image so it is divisible by 8 in both dimensions
   m,n = img.shape
 
-  for i in range(0, m, 8):
-    for j in range(0, n, 8):
+  h_pad = (m % 8)
+  v_pad = (n % 8)
+  pad_img = np.pad(img, ((0, v_pad), (0, h_pad)), "constant", constant_values=0)
+
+  m2,n2 = pad_img.shape
+  G = np.zeros(pad_img.shape)
+
+  for i in range(0, m2, 8):
+    for j in range(0, n2, 8):
       # apply DCT for every 8x8 block
-      b = img[i:i+8, j:j+8]
+      b = pad_img[i:i+8, j:j+8]
 
       b_dct = dctn(b, norm='ortho')
 
-      print(b_dct)
-
       # Quantize using the quantization table provided, rounding values to integers
+      # TODO: check if round is necessary (since we are not worried about saving bits)
+      # though rounding to 0 might be useful
       b_qntz = np.round(np.divide(b_dct, q_table))
-      print(b_qntz)
-
-      print(np.multiply(b_qntz, q_table))
 
 
+      G[i:i+8, j:j+8] = b_qntz
+  
+  return G
 
+def img_recov(img, q_table, original_shape):
+  m,n = img.shape
 
+  r_img = np.zeros(img.shape)
+
+  for i in range(0, m, 8):
+    for j in range(0, n, 8):
+      b = img[i:i+8, j:j+8]
+
+      # get coeficients back
+      b_dct = np.multiply(b, q_table)
+
+      # apply idct type 2 (DCT III) to get the image in the spatial domain
+      # r_b: recovered block
+      r_b = idctn(b_dct, norm='ortho')
+
+      r_img[i:i+8, j:j+8] = r_b
+
+  o_w, o_h = original_shape
+  # shift back the image
+  shifted_r_img = np.add(r_img[:o_w, :o_h], 128).astype(np.uint8)
+
+  return shifted_r_img
 
 def rmse_compare(A, B):
   assert A.shape == B.shape, "Shapes not compatible"
@@ -67,34 +94,45 @@ def rmse_compare(A, B):
 # filename = input().strip()
 # img = imageio.imread(filename).astype(np.int8)
 
-
 # TEST 1
-test = np.array([
-  [62,55,55,54,49,48,47,55],
-  [62,57,54,52,48,47,48,53],
-  [61,60,52,49,48,47,49,54],
-  [63,61,60,60,63,65,68,65],
-  [67,67,70,74,79,85,91,92],
-  [82,95,101,106,114,115,112,117],
-  [96,111,115,119,128,128,130,127],
-  [109,121,127,133,139,141,140,133],
-])
-
-# TEST 2 (from wikipedia)
 # test = np.array([
-#   [52,55,61,66,70,61,64,73],
-#   [63,59,55,90,109,85,69,72],
-#   [62,59,68,113,144,104,66,73],
-#   [63,58,71,122,154,106,70,69],
-#   [67,61,68,104,126,88,68,70],
-#   [79,65,60,70,77,68,58,75],
-#   [85,71,64,59,55,61,65,83],
-#   [87,79,69,68,65,76,78,94],
+#   [62,55,55,54,49,48,47,55],
+#   [62,57,54,52,48,47,48,53],
+#   [61,60,52,49,48,47,49,54],
+#   [63,61,60,60,63,65,68,65],
+#   [67,67,70,74,79,85,91,92],
+#   [82,95,101,106,114,115,112,117],
+#   [96,111,115,119,128,128,130,127],
+#   [109,121,127,133,139,141,140,133],
 # ])
 
-img_dct(test, QUANTIZATION_TABLE)
+# TEST 2 (DIP (Gonzalez, Woods) 3rd ed., page. 383)
+test = np.array([
+  [52,55,61,66,70,61,64,73],
+  [63,59,66,90,109,85,69,72],
+  [62,59,68,113,144,104,66,73],
+  [63,58,71,122,154,106,70,69],
+  [67,61,68,104,126,88,68,70],
+  [79,65,60,70,77,63,58,75],
+  [85,71,64,59,55,61,65,83],
+  [87,79,69,68,65,76,78,94],
+])
+
+dcted_image = img_dct(test, QUANTIZATION_TABLE)
+
+r_img = img_recov(dcted_image, QUANTIZATION_TABLE, test.shape)
+
+plt.subplot(211)
+plt.imshow(test)
+
+plt.subplot(212)
+plt.imshow(r_img)
+
+plt.show()
 
 # Divide the image into 8x8 blocks
 # Transform each block using DCT mathematical operations
 # Quantitize each DCT block (lossy compression)
 # Embed the message bits from the quantitized coefficients (avoid 0, 1, -1, and the AC)
+
+# luminance/chrominance
